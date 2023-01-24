c6aa64b0caf0dd80322e0659e12905d338429c4b
eltonalexanderajadi <eltonalexander16@gmail.com>
2023-01-24 14:50:39 -0800

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,25 +11,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-  size_t i;
-  int tmp, sorted;
+size_t i;
+int tmp, sorted;
 
-  if (!array || size == 0)
-    return;
+if (!array || size == 0)
+return;
 
-  do {
-    sorted = 1;
-    for (i = 0; i < size - 1; i++)
-      {
-	if (array[i] > array[i + 1])
-	  {
-	    sorted = 0;
-	    tmp = array[i];
-	    array[i] = array[i + 1];
-	    array[i + 1] = tmp;
-	    print_array(array, size);
-	  }
-      }
+do {
+sorted = 1;
+for (i = 0; i < size - 1; i++)
+{
+if(array[i] > array[i + 1])
+{
+sorted = 0;
+tmp = array[i];
+array[i] = array[i + 1];
+array[i + 1] = tmp;
+print_array(array, size);
+}
+}
 
-  } while (sorted == 0);
+} while (sorted == 0);
 }
